9beb25140bc015dabf120867392474f289eefb72
kh4l1d <khalidahmedsayed@gmail.com>
2016-10-01 18:42:58 +0530

    Generate random text of specified length

--- TextGenerator.py
+++ TextGenerator.py
@@ -1,10 +1,23 @@
 import random,string
 
-def generator():
-    letter1 = random.choice(string.ascii_letters)
-    letter2 = random.choice(string.ascii_letters)
-    letter3 = random.choice(string.ascii_letters)
-    word = letter1 + letter2 + letter3
-    return word
+def generator(n):
+    i = 0
+    textList = []
+    text = ''
+    while i<n:
+        textList.insert(i,random.choice(string.ascii_letters))
+        text = text + textList[i]
+        i = i + 1
 
-print(generator())
+    return text
+
+
+
+n = input('Enter the length of the random text you want : ')
+n = int(n)
+
+print(generator(n))
+
+#textLength = n*(random.random())
+#textLength = int(textLength)
+#letter2 = random.choice(string.ascii_letters)
