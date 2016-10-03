0b700079cd58ee9c94aa2b3b299af5a28008f0ba
kh4l1d <khalidahmedsayed@gmail.com>
2016-10-03 19:38:24 +0530

    Added GUI

--- TextGenerator.py
+++ TextGenerator.py
@@ -1,6 +1,11 @@
-import random,string
+import random,string,Tkinter
+from Tkinter import *
 
-def generator(n,s):
+window = Tk()
+
+def generator():
+    n = int(e1Value.get())
+    s = float(e2Value.get())
     i = 0
     textList = []
     text = ''
@@ -13,11 +18,30 @@ def generator(n,s):
         else:
             text = text + ' '
 
-    return text
+    t1.insert(END,text)
 
-n = input('Enter the length of the random text you want : ')
-n = int(n)
-s = input('Enter the frequency [0,1) of spaces you want : ')
-s = float(s)
 
-print(generator(n,s))
+l1 = Label(window,text='Number of characters')
+l1.grid(row=0,column=0)
+
+l2 = Label(window,text='Frequency [0,1) of spaces')
+l2.grid(row=3,column=0)
+
+
+e1Value = StringVar()
+e1 = Entry(window,textvariable=e1Value)
+e1.grid(row=0,column=1)
+
+
+e2Value = StringVar()
+e2 = Entry(window,textvariable=e2Value)
+e2.grid(row=3,column=1)
+
+
+b1 = Button(window,text = "Generate",command=generator)
+b1.grid(row=5,column=1)
+
+t1 = Text(window,height=60,width=30)
+t1.grid(row=7,column=0)
+
+window.mainloop()
